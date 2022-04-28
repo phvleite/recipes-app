640c6bed6210970dbb60e69fa9ea82949bdeca73
phvleite <phvleite@gmail.com>
2022-04-28 11:39:38 -0300

    corrigido erro header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import ProfileIcon from '../images/profileIcon.svg';
