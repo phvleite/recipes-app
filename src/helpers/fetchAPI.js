6231efccb4a87a4b27804293f43427122f24064b
Adolligit <adelson.onod@gmail.com>
2022-05-03 19:49:03 -0300

    Organizando 'fetchAPI' para facilitar na busca pelos endpoints

--- src/helpers/fetchAPI.js
+++ src/helpers/fetchAPI.js
@@ -11,29 +11,29 @@ const fetchByCategories = async (mealOrDrink, category) => {
 
 const fetchAPI = async (chooseUrl, query) => {
   const verifyUrl = {
-    fetchMealByIngredient: `https://www.themealdb.com/api/json/v1/1/filter.php?i=${query}`,
-
-    fetchMealByFirstLetter: `https://www.themealdb.com/api/json/v1/1/search.php?f=${query}`,
-
-    fetchMealById: `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${query}`,
-
+    // Nome
     fetchMealByName: `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`,
-
-    fetchCocktailById: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${query}`,
-
-    fetchCocktailByIngredient: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${query}`,
-
     fetchCocktailByName: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`,
 
+    // ID
+    fetchMealById: `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${query}`,
+    fetchCocktailById: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${query}`,
+
+    // Ingrediente
+    fetchMealByIngredient: `https://www.themealdb.com/api/json/v1/1/filter.php?i=${query}`,
+    fetchCocktailByIngredient: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${query}`,
+
+    // Categoria
+    fetchMealListCategory: 'https://www.themealdb.com/api/json/v1/1/list.php?c=list',
+    fetchCocktailListCategory: 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list',
+
+    // Primeira letra
+    fetchMealByFirstLetter: `https://www.themealdb.com/api/json/v1/1/search.php?f=${query}`,
     fetchCocktailByFirstLetter: `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${query}`,
 
+    // Lista de ingredientes
     fetchMealIngredientsByList: 'https://www.themealdb.com/api/json/v1/1/list.php?i=list',
-
     fetchCocktailIngredientsByList: 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list',
-
-    fetchMealListCategory: 'https://www.themealdb.com/api/json/v1/1/list.php?c=list',
-
-    fetchCocktailListCategory: 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list',
   };
 
   if (query.length !== 1
