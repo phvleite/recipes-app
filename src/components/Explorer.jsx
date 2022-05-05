ca38ab0c83d12a54505fb9b3777e51dba463d5aa
phvleite <phvleite@gmail.com>
2022-05-05 00:00:02 -0300

    [REQ70/71/72/73/74] Aprovados

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { fetchAPI } from '../helpers/fetchAPI';
 
 function Explorer(props) {
-  console.log(props);
   const { linkTo, history } = props;
+  const [recipeSurprise, setRecipeSurprise] = useState({});
   const isFood = linkTo === 'foods';
-  console.log(history);
 
   const btnNationality = (
     <button
       className="btn-explore-food"
-      data-testid="explore-drinks"
+      data-testid="explore-by-nationality"
       type="button"
       onClick={ () => history.push('/explore/foods/nationalities') }
     >
@@ -24,14 +24,45 @@ function Explorer(props) {
     </button>
   );
 
+  function goToSurpriseMe() {
+    let linkSurprise = '';
+    if (linkTo === 'foods') {
+      linkSurprise = `/${linkTo}/${recipeSurprise.idMeal}`;
+      if (linkSurprise !== '/foods/undefined') {
+        history.push(linkSurprise);
+      }
+    } else {
+      linkSurprise = `/${linkTo}/${recipeSurprise.idDrink}`;
+      if (linkSurprise !== '/drinks/undefined') {
+        history.push(linkSurprise);
+      }
+    }
+  }
+
+  async function getSurpriseMe() {
+    if (recipeSurprise.length > 0) {
+      recipeSurprise.splice(0, recipeSurprise.length);
+    }
+    if (linkTo === 'foods') {
+      fetchAPI('fetchMealSurpriseMe', '').then((data) => setRecipeSurprise(...data));
+    } else {
+      fetchAPI('fetchCocktailSurpriseMe', '')
+        .then((data) => setRecipeSurprise(...data));
+    }
+  }
+
+  useEffect(() => {
+    goToSurpriseMe();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [recipeSurprise]);
+
   return (
     <div className="box-explore">
       <button
         className="btn-explore-food"
         data-testid="explore-by-ingredient"
         type="button"
-        // onClick={ () => history.push(`/explore/${linkTo}/ingredients`) }
-        onClick={ () => history.push('/explore/foods/ingredients') }
+        onClick={ () => history.push(`/explore/${linkTo}/ingredients`) }
       >
         <div className="title-btn-explore">
           By Ingredient
@@ -46,7 +77,7 @@ function Explorer(props) {
         className="btn-explore-food"
         data-testid="explore-surprise"
         type="button"
-        // onClick={ () => history.push('/explore/foods/nationalities') }
+        onClick={ getSurpriseMe }
       >
         <div className="title-btn-explore">
           Surprise me!
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Explorer from '../components/Explorer';
 
-function ExploreFoods() {
+function ExploreFoods({ history }) {
   return (
     <>
       <Header title="Explore Foods" />
-      <Explorer linkTo="foods" />
+      <Explorer linkTo="foods" history={ history } />
       <Footer />
     </>
   );
 }
+
+ExploreFoods.propTypes = {
+  history: PropTypes.objectOf(),
+}.isRequerid;
 
 export default ExploreFoods;
