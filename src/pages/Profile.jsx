e47f66ca18b777b57295bc41727b849a2e8666ff
Adolligit <adelson.onod@gmail.com>
2022-05-05 17:06:52 -0300

    Links inseridos nos botões da página

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-function Profile() {
+function Profile({ history }) {
   const [email, setEmail] = useState('');
 
   useEffect(() => setEmail(JSON.parse(localStorage.user).email), []);
@@ -11,29 +12,35 @@ function Profile() {
     <>
       <Header title="Profile" />
       <h3 data-testid="profile-email">{ email }</h3>
-      <button type="button">Click Me!</button>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-      >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-      >
-        Logout
-      </button>
+      <div id="buttons-profile">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => { localStorage.clear(); history.push('/'); } }
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
       { console.log(email) }
     </>
   );
 }
+
+Profile.propTypes = { history: PropTypes.objectOf() }.isRequered;
 
 export default Profile;
